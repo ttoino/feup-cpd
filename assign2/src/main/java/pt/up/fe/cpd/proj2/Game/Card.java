eb8ac1c5c6c956f9d0d0ff87c9c4432470008cf7
Matias Vaz <matiasfreitasguimaraes@gmail.com>
2023-05-03 09:49:45 +0100

    Card class basic functions created

---
+++ assign2/src/main/java/pt/up/fe/cpd/proj2/Game/Card.java
@@ -0,0 +1,65 @@
+public class Card {
+
+    String symbol;
+    int value;
+
+    Card(String symbol){
+        switch (symbol) {
+            case "Ace":
+                this.symbol = symbol;
+                this.value = 11;
+            case "2":
+                this.symbol = symbol;
+                this.value = 2;
+            case "3":
+                this.symbol = symbol;
+                this.value = 3;
+            case "4":
+                this.symbol = symbol;
+                this.value = 4;
+            case "5":
+                this.symbol = symbol;
+                this.value = 5;
+            case "6":
+                this.symbol = symbol;
+                this.value = 6;
+            case "7":
+                this.symbol = symbol;
+                this.value = 7;
+            case "8":
+                this.symbol = symbol;
+                this.value = 8;
+            case "9":
+                this.symbol = symbol;
+                this.value = 9;
+            case "10":
+                this.symbol = symbol;
+                this.value = 10;
+            case "Jack":
+                this.symbol = symbol;
+                this.value = 10;
+            case "Queen":
+                this.symbol = symbol;
+                this.value = 10;
+            case "King":
+                this.symbol = symbol;
+                this.value = 10;
+            default:
+                throw new Exception("Invalid Card Number");
+
+
+        }
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void changeAceValue {
+        this.value = 1;
+    }
+}
